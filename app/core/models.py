1a0d880f9cded3fccaefe595af7e401a4025ba41
Melissa Gelfs <melissa@email.com>
2023-03-03 09:06:04 +0000

    User model v2

--- app/core/models.py
+++ app/core/models.py
@@ -1,5 +1,5 @@
 """
-Database models
+Database models.
 """
 from django.db import models
 from django.contrib.auth.models import (
@@ -13,7 +13,7 @@ class UserManager(BaseUserManager):
     """Manager for users."""
 
     def create_user(self, email, password=None, **extra_fields):
-        """Create, save and return new user"""
+        """Create, save and return a new user."""
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
